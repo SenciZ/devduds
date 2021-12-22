dc9ae270cd7b1aa92621e10d29cbc772dbe8a0b2
Senad Zenkovic <zenkovic88@gmail.com>
2021-12-22 08:06:11 -0700

    connect server

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, '../index.html'))
+})
+
+app.get('/css', (req, res)=>{
+    res.sendFile(path.join(__dirname, '../style.css'))
+})
+
+app.get('/cssreset', (req, res)=>{
+    res.sendFile(path.join(__dirname, '../reset.css'))
+})
+
+app.get('/js', (req, res)=>{
+    res.sendFile(path.join(__dirname, '../main.js'))
+})
+
+
+const port = process.env.PORT || 4005
+
+app.listen(port, ()=>{console.log(`Listenting on port ${port}`)})
