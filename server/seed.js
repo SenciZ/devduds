b6b330bd385bcdca6141dcd522bbb038b4042924
Senad Zenkovic <zenkovic88@gmail.com>
2021-12-29 14:27:13 -0500

    add new shirts

--- server/seed.js
+++ server/seed.js
@@ -42,10 +42,10 @@ module.exports = {
 
         insert into products (product_name, product_description, product_price, image_url, product_category_id)
         values 
-        ('Favorite Framework Shirt', 'You know React is your favorite', '12.99', 'https://github.com/SenciZ/devduds/blob/main/resources/reactShirt.png?raw=true', '1'),
-        ('T-shirt React', 'Only true devs use react, this shirt proves that you are a true dev', '19.99', 'https://github.com/SenciZ/devduds/blob/main/resources/reactShirt.png?raw=true', '1'),
-        ('React T-Shirt', 'A soft wonderful shirt with your favorite framework logo.. React!', '16.99', 'https://github.com/SenciZ/devduds/blob/main/resources/reactShirt.png?raw=true', '1'),
-        ('Another React T-Shirt', 'This shirt is better than the other React Shirt, get it', '12.99', 'https://github.com/SenciZ/devduds/blob/main/resources/reactShirt.png?raw=true', '1')
+        ('Black Angular T-Shirt', 'You know React is your favorite', '12.99', 'https://github.com/SenciZ/devduds/blob/main/resources/Products/angular-black.png?raw=true', '1'),
+        ('Black Angular T-Shirt', 'You know React is your favorite', '12.99', 'https://github.com/SenciZ/devduds/blob/main/resources/Products/angular-black.png?raw=true', '1'),
+        ('Black Angular T-Shirt', 'You know React is your favorite', '12.99', 'https://github.com/SenciZ/devduds/blob/main/resources/Products/angular-black.png?raw=true', '1'),
+        ('Black Angular T-Shirt', 'You know React is your favorite', '12.99', 'https://github.com/SenciZ/devduds/blob/main/resources/Products/angular-black.png?raw=true', '1')
         ;`)
       .then(() => {
         console.log("DB seeded!");
